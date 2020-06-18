d92c9559b1ced21cc3773e72f9a9007b368d8369
tlbn <tete.leticia@gmail.com>
2020-06-18 12:42:55 -0300

    database seeding

--- src/main/java/com/mylibrary/livraria/user/UserRepository.java
+++ src/main/java/com/mylibrary/livraria/user/UserRepository.java
@@ -1,4 +1,6 @@
 package com.mylibrary.livraria.user;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
